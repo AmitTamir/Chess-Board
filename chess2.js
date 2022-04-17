ea2ae8340c429a1493d82808cb6d6d4f3ad20341
amit tamir <amit.tamir01@gmail.com>
2022-04-17 13:54:02 +0300

    update

--- chess2.js
+++ chess2.js
@@ -1,31 +1,43 @@
 window.addEventListener('load', () => {
     const body = document.getElementsByTagName("body")[0];
-    const table = document.createElement("table");
-    table.className = "table";
+    const board = document.createElement("table");
+    board.className = "board";
     for (let t = 0; t < 8; t++) {
         let row = document.createElement('tr');
         for (let i = 0; i < 8; i++) {
             let cell = document.createElement('td');
             if (t % 2 === 0) {
                 if (i % 2 === 0) {
-                    cell.className = "white";
+                    cell.className = "light";
                 }
                 else {
-                    cell.className = "black";
+                    cell.className = "dark";
                 }
             }
             else {
                 if (i % 2 === 0) {
-                    cell.className = "black";
+                    cell.className = "dark";
                 }
                 else {
-                    cell.className = "white";
+                    cell.className = "light";
                 }
 
             }
-            table.appendChild(row);
+            board.appendChild(row);
             row.appendChild(cell);
         }
-        body.appendChild(table);
+        body.appendChild(board);
+    }
+    const whitePieces = document.getElementsByClassName('whitePieces');
+    board.appendChild(whitePieces);
+    const blackPieces = document.getElementsByClassName('blackPieces');
+    board.appendChild(blackPieces);
+    // const btn = document.getElementsByClassName('item');
+    // btn.addEventListener('click', () => {
+    //     target.chess.css.backgroundColor = 'salmon';
+    // })
+    function myPieceClick() {
+        document.getElementsByTagName("td").style.color = "blue";
     }
 });
+
