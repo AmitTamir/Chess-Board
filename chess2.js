f7a7769418044a294592a451c988176f85555d65
amit tamir <amit.tamir01@gmail.com>
2022-04-18 17:03:46 +0300

    update

--- chess2.js
+++ chess2.js
@@ -3,7 +3,180 @@ function getImage(cell, type, kind) {
     image.src = "pieces/" + type + kind + ".png";
     cell.appendChild(image);
 }
-window.addEventListener('load', () => {
+let selected;
+let pieces = [];
+function cellClick(e) {
+    if (selected !== undefined) {
+        selected.classList.remove('clicked');
+    }
+    const moveArray = [];
+    let row = 1;
+    let coll = 6;
+    if (e.type === " b") {
+        let temp1 = coll + 1;
+        let temp2 = coll - 1;
+        for (let i = row - 1; i > -1; i--) {
+            if (temp1 < 8) {
+                moveArray.push(document.getElementById(`${i}-${temp1}`));
+            }
+            if (temp2 > -1) {
+                moveArray.push(document.getElementById(`${i}-${temp2}`));
+            }
+            temp1++;
+            temp2--;
+        }
+        temp1 = coll + 1;
+        temp2 = coll - 1;
+        for (let i = row + 1; i < 8; i++) {
+            if (temp1 < 8) {
+                moveArray.push(document.getElementById(`${i}-${temp1}`));
+            }
+            if (temp2 > -1) {
+                moveArray.push(document.getElementById(`${i}-${temp2}`));
+            }
+            temp1++;
+            temp2--;
+        }
+    }
+    if (e.type === " q") {
+        let temp1 = coll + 1;
+        let temp2 = coll - 1;
+        for (let i = row - 1; i > -1; i--) {
+            if (temp1 < 8) {
+                moveArray.push(document.getElementById(`${i}-${temp1}`));
+            }
+            if (temp2 > -1) {
+                moveArray.push(document.getElementById(`${i}-${temp2}`));
+            }
+            temp1++;
+            temp2--;
+        }
+        temp1 = coll + 1;
+        temp2 = coll - 1;
+        for (let i = row + 1; i < 8; i++) {
+            if (temp1 < 8) {
+                moveArray.push(document.getElementById(`${i}-${temp1}`));
+            }
+            if (temp2 > -1) {
+                moveArray.push(document.getElementById(`${i}-${temp2}`));
+            }
+            temp1++;
+            temp2--;
+        }
+        temp1 = coll + 1;
+        temp2 = coll - 1;
+        for (let i = 0; i < 7; i++) {
+            if (temp1 < 8) {
+                moveArray.push(document.getElementById(`${row}-${temp1}`));
+            }
+            if (temp2 > -1) {
+                moveArray.push(document.getElementById(`${row}-${temp2}`));
+            }
+            temp1++;
+            temp2--;
+        }
+        temp1 = row + 1;
+        temp2 = row - 1;
+        for (let i = 0; i < 7; i++) {
+            if (temp2 > -1) {
+                moveArray.push(document.getElementById(`${temp2}-${coll}`));
+            }
+            if (temp1 < 8) {
+                moveArray.push(document.getElementById(`${temp1}-${coll}`));
+            }
+            temp1++;
+            temp2--;
+        }
+    }
+    if (e.type === " r") {
+        console.log("rock moves");
+        temp1 = coll + 1;
+        temp2 = coll - 1;
+        for (let i = 0; i < 7; i++) {
+            if (temp1 < 8) {
+                moveArray.push(document.getElementById(`${row}-${temp1}`));
+            }
+            if (temp2 > -1) {
+                moveArray.push(document.getElementById(`${row}-${temp2}`));
+            }
+            temp1++;
+            temp2--;
+        }
+        temp1 = row + 1;
+        temp2 = row - 1;
+        for (let i = 0; i < 7; i++) {
+            if (temp2 > -1) {
+                moveArray.push(document.getElementById(`${temp2}-${coll}`));
+            }
+            if (temp1 < 8) {
+                moveArray.push(document.getElementById(`${temp1}-${coll}`));
+            }
+            temp1++;
+            temp2--;
+        }
+    }
+    if ((e.type === " p") && (e.color === "white")) {
+        console.log("white pawn moves");
+        moveArray.push(document.getElementById(`${row - 1}-${coll}`));
+        moveArray.push(document.getElementById(`${row - 2}-${coll}`));
+    }
+    // if ((e.type === " p") && (e.color === "black")) {
+    console.log("black pawn moves");
+    moveArray.push(document.getElementById(`${row + 1}-${coll}`));
+    moveArray.push(document.getElementById(`${row + 2}-${coll}`));
+    // }
+
+
+    console.log(moveArray);
+    selected = e.currentTarget;
+    selected.classList.add('clicked');
+
+}
+class piece {
+    constructor(row, coll, type, color) {
+        this.row = row;
+        this.coll = coll;
+        this.type = type;
+        this.color = color;
+    }
+}
+function getInitialBoard() {
+    let result = [];
+    result.push(new piece(0, 0, " r", "black"));
+    result.push(new piece(0, 1, " kn", "black"));
+    result.push(new piece(0, 2, " b", "black"));
+    result.push(new piece(0, 3, " q", "black"));
+    result.push(new piece(0, 4, " k", "black"));
+    result.push(new piece(0, 5, " b", "black"));
+    result.push(new piece(0, 6, " kn", "black"));
+    result.push(new piece(0, 7, " r", "black"));
+    result.push(new piece(1, 0, " p", "black"));
+    result.push(new piece(1, 1, " p", "black"));
+    result.push(new piece(1, 2, " p", "black"));
+    result.push(new piece(1, 3, " p", "black"));
+    result.push(new piece(1, 4, " p", "black"));
+    result.push(new piece(1, 5, " p", "black"));
+    result.push(new piece(1, 6, " p", "black"));
+    result.push(new piece(1, 7, " p", "black"));
+    result.push(new piece(7, 0, " r", "white"));
+    result.push(new piece(7, 1, " kn", "white"));
+    result.push(new piece(7, 2, " b", "white"));
+    result.push(new piece(7, 3, " q", "white"));
+    result.push(new piece(7, 4, " k", "white"));
+    result.push(new piece(7, 5, " b", "white"));
+    result.push(new piece(7, 6, " kn", "white"));
+    result.push(new piece(7, 7, " r", "white"));
+    result.push(new piece(6, 0, " p", "white"));
+    result.push(new piece(6, 1, " p", "white"));
+    result.push(new piece(6, 2, " p", "white"));
+    result.push(new piece(6, 3, " p", "white"));
+    result.push(new piece(6, 4, " p", "white"));
+    result.push(new piece(6, 5, " p", "white"));
+    result.push(new piece(6, 6, " p", "white"));
+    result.push(new piece(6, 7, " p", "white"));
+    return result;
+}
+function createChessBoard() {
     const body = document.getElementsByTagName("body")[0];
     const board = document.createElement("table");
     board.className = "board";
@@ -26,56 +199,18 @@ window.addEventListener('load', () => {
                 else {
                     cell.className = "light";
                 }
-
             }
+            cell.addEventListener('click', cellClick);
             board.appendChild(row);
             row.appendChild(cell);
-            if (t === 1) {
-                getImage(cell, "black", " p");
-            }
-            else if (t === 6) {
-                getImage(cell, "white", " p");
-            }
-            else if (t === 0) {
-                if (i === 0 || i === 7) {
-                    getImage(cell, "black", " r");
-                }
-                else if (i === 1 || i === 6) {
-                    getImage(cell, "black", " kn");
-                }
-                else if (i === 2 || i === 5) {
-                    getImage(cell, "black", " b");
-                }
-                else if (i === 3) {
-                    getImage(cell, "black", " q");
-                }
-                else if (i === 4) {
-                    getImage(cell, "black", " k");
-                }
-            }
-            else if (t === 7) {
-                if (i === 0 || i === 7) {
-                    getImage(cell, "white", " r");
-                }
-                else if (i === 1 || i === 6) {
-                    getImage(cell, "white", " kn");
-                }
-                else if (i === 2 || i === 5) {
-                    getImage(cell, "white", " b");
-                }
-                else if (i === 3) {
-                    getImage(cell, "white", " q");
-                }
-                else if (i === 4) {
-                    getImage(cell, "white", " k");
-                }
-            }
+            cell.id = t + "-" + i
         }
         body.appendChild(board);
     }
-});
-let clicktry = document.getElementsByClassName('img')[0];
-clicktry.addEventListener('click', function (e) {
-    e.target.style.outline = "solid blue 1px";
-});
+    pieces = getInitialBoard();
+    for (let piece of pieces) {
+        getImage(board.rows[piece.row].cells[piece.coll], piece.color, piece.type);
+    }
+}
+window.addEventListener('load', createChessBoard);
 
