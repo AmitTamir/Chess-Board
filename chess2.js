4a08703b785962f5925f327d852452dcf452ac40
amit tamir <amit.tamir01@gmail.com>
2022-04-18 09:29:20 +0300

    update

--- chess2.js
+++ chess2.js
@@ -1,3 +1,8 @@
+function getImage(cell, type, kind) {
+    const image = document.createElement("img");
+    image.src = "pieces/" + type + kind + ".png";
+    cell.appendChild(image);
+}
 window.addEventListener('load', () => {
     const body = document.getElementsByTagName("body")[0];
     const board = document.createElement("table");
@@ -25,19 +30,52 @@ window.addEventListener('load', () => {
             }
             board.appendChild(row);
             row.appendChild(cell);
+            if (t === 1) {
+                getImage(cell, "black", " p");
+            }
+            else if (t === 6) {
+                getImage(cell, "white", " p");
+            }
+            else if (t === 0) {
+                if (i === 0 || i === 7) {
+                    getImage(cell, "black", " r");
+                }
+                else if (i === 1 || i === 6) {
+                    getImage(cell, "black", " kn");
+                }
+                else if (i === 2 || i === 5) {
+                    getImage(cell, "black", " b");
+                }
+                else if (i === 3) {
+                    getImage(cell, "black", " q");
+                }
+                else if (i === 4) {
+                    getImage(cell, "black", " k");
+                }
+            }
+            else if (t === 7) {
+                if (i === 0 || i === 7) {
+                    getImage(cell, "white", " r");
+                }
+                else if (i === 1 || i === 6) {
+                    getImage(cell, "white", " kn");
+                }
+                else if (i === 2 || i === 5) {
+                    getImage(cell, "white", " b");
+                }
+                else if (i === 3) {
+                    getImage(cell, "white", " q");
+                }
+                else if (i === 4) {
+                    getImage(cell, "white", " k");
+                }
+            }
         }
         body.appendChild(board);
     }
-    const whitePieces = document.getElementsByClassName('whitePieces');
-    board.appendChild(whitePieces);
-    const blackPieces = document.getElementsByClassName('blackPieces');
-    board.appendChild(blackPieces);
-    // const btn = document.getElementsByClassName('item');
-    // btn.addEventListener('click', () => {
-    //     target.chess.css.backgroundColor = 'salmon';
-    // })
-    function myPieceClick() {
-        document.getElementsByTagName("td").style.color = "blue";
-    }
+});
+let clicktry = document.getElementsByClassName('img')[0];
+clicktry.addEventListener('click', function (e) {
+    e.target.style.outline = "solid blue 1px";
 });
 
